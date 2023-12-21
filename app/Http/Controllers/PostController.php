17d931a787d75c56b37b2c45319fb0d5c545c11a
Natsu718 <o.natsumi1216@gmail.com>
2023-12-21 09:39:54 +0900

    カリキュラム8-2

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -1,14 +1,15 @@
 <?php
 
 namespace App\Http\Controllers;
-use App\Http\Controllers\PostController;
 
-use Illuminate\Http\Request;
 use App\Models\Post;
+use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    public function index(Post $post){
-        return $post->get();
+    public function index(Post $post)
+    {
+        return view('posts.index')->with(['posts' => $post->getPaginateByLimit()]);
     }
 }
+?>
